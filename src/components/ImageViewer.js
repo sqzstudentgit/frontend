349d72aa904410257a3d5f8730cd5a12081d5679
Nabeel Zafar <nabeel.zafar@student.unimelb.edu.au>
2020-10-04 22:43:23 +1100

    Show 3D model from the DB.

--- src/components/ImageViewer.js
+++ src/components/ImageViewer.js
@@ -7,36 +7,70 @@ const { TabPane } = Tabs;
 class ImageViewer extends React.Component {
 
     render() {
-        console.log(this.props.imageList);
-        console.log('height: '+this.props.height)
-
-        return (
-            <div style={{height: this.props.height, width:this.props.width}} >
-                <Tabs tabPosition="left" style={{ height: this.props.height }}>
-                    <TabPane tab={<img width="70" height="70" style = {{borderRadius: "10%"}} src="https://attachments.holyoake.com/products/images_medium/2117.jpg"/>} key="1" >
-                        <img style = {{maxHeight: this.props.height,borderRadius: "5%"}}  src = "https://attachments.holyoake.com/products/images_medium/2117.jpg"/>
+        
+        if(this.props.imageList==null) {return (<></>);}
+        else
+        {
+            console.log('imageList: ');
+            console.log(this.props.imageList);
+            const tabList = this.props.imageList.map((imageInfo) => {
+                return imageInfo.is3DModelType=="N" 
+                ?
+                    <TabPane key={imageInfo.id.toString()} tab={<img width="70" height="70" style = {{borderRadius: "10%"}}  src={imageInfo.smallImageLocation}/>} >
+                        <img style = {{height: this.props.height,borderRadius: "5%"}}  src = {imageInfo.largeImageLocation}/>
                     </TabPane>
-                    <TabPane tab={<img width="70" height="70" style = {{borderRadius: "10%"}} src="https://attachments.holyoake.com/products/images_large/2115.jpg?r=1334023288"/>} key="2">
-                        <img style = {{ maxHeight: this.props.height, borderRadius: "5%"}}  src = "https://attachments.holyoake.com/products/images_large/2115.jpg?r=1334023288"/>
-                    </TabPane>
-                    <TabPane tab={<img width="70" height="70" style = {{borderRadius: "10%"}} src="https://attachments.holyoake.com/products/images_large/2116.jpg?r=1334023306"/>} key="3">
-                        <img style = {{ maxHeight: this.props.height, borderRadius: "5%"}} src = "https://attachments.holyoake.com/products/images_large/2116.jpg?r=1334023306"/>
-                    </TabPane>
-                    <TabPane tab={<img width="70" height="70" style = {{borderRadius: "10%"}} src="https://attachments.holyoake.com/products/images_large/2115.jpg?r=1334023288"/>} key="4">
-                        <img style = {{ maxHeight: this.props.height, borderRadius: "5%"}}  src = "https://attachments.holyoake.com/products/images_large/2115.jpg?r=1334023288"/>
-                    </TabPane>
-                    <TabPane tab={<img width="70" height="70" style = {{borderRadius: "10%"}} src="https://attachments.holyoake.com/products/images_large/2116.jpg?r=1334023306"/>} key="5">
-                        <img style = {{ maxHeight: this.props.height, borderRadius: "5%"}} src = "https://attachments.holyoake.com/products/images_large/2116.jpg?r=1334023306"/>
-                    </TabPane>
-
+                :
                     <TabPane tab="3D Model" key="6">
                         <div style={{height:this.props.height, width:'100%'}} >
-                            <ThreeDModelPresenter modelUrl="https://holysas-3d-images.s3.amazonaws.com/model2.glb" />
+                            <ThreeDModelPresenter modelUrl={imageInfo.threeDModelLocation} />
                         </div>
                     </TabPane>
-                </Tabs>
-            </div>
-        )
+                }
+            );
+            /*const tabList = this.props.imageList.map((imageInfo) =>
+                <p key={imageInfo.id.toString()}> {imageInfo.fileName} </p>
+            );*/
+            console.log(tabList)
+
+            return (
+                <div style={{height: this.props.height, width:this.props.width}} >
+                    
+
+                    <Tabs tabPosition="left" style={{ height: this.props.height }}>
+                        {tabList}
+                    </Tabs>
+                </div>
+            )
+
+            return (
+                <div style={{height: this.props.height, width:this.props.width}} >
+                    <Tabs tabPosition="left" style={{ height: this.props.height }}>
+                        <TabPane tab={<img width="70" height="70" style = {{borderRadius: "10%"}} src="https://attachments.holyoake.com/products/images_medium/2117.jpg"/>} key="1" >
+                            <img style = {{maxHeight: this.props.height,borderRadius: "5%"}}  src = "https://attachments.holyoake.com/products/images_medium/2117.jpg"/>
+                        </TabPane>
+                        <TabPane tab={<img width="70" height="70" style = {{borderRadius: "10%"}} src="https://attachments.holyoake.com/products/images_large/2115.jpg?r=1334023288"/>} key="2">
+                            <img style = {{ maxHeight: this.props.height, borderRadius: "5%"}}  src = "https://attachments.holyoake.com/products/images_large/2115.jpg?r=1334023288"/>
+                        </TabPane>
+                        <TabPane tab={<img width="70" height="70" style = {{borderRadius: "10%"}} src="https://attachments.holyoake.com/products/images_large/2116.jpg?r=1334023306"/>} key="3">
+                            <img style = {{ maxHeight: this.props.height, borderRadius: "5%"}} src = "https://attachments.holyoake.com/products/images_large/2116.jpg?r=1334023306"/>
+                        </TabPane>
+                        <TabPane tab={<img width="70" height="70" style = {{borderRadius: "10%"}} src="https://attachments.holyoake.com/products/images_large/2115.jpg?r=1334023288"/>} key="4">
+                            <img style = {{ maxHeight: this.props.height, borderRadius: "5%"}}  src = "https://attachments.holyoake.com/products/images_large/2115.jpg?r=1334023288"/>
+                        </TabPane>
+                        <TabPane tab={<img width="70" height="70" style = {{borderRadius: "10%"}} src="https://attachments.holyoake.com/products/images_large/2116.jpg?r=1334023306"/>} key="5">
+                            <img style = {{ maxHeight: this.props.height, borderRadius: "5%"}} src = "https://attachments.holyoake.com/products/images_large/2116.jpg?r=1334023306"/>
+                        </TabPane>
+
+                        <TabPane tab="3D Model" key="6">
+                            <div style={{height:this.props.height, width:'100%'}} >
+                                <ThreeDModelPresenter modelUrl="https://holysas-3d-images.s3.amazonaws.com/model2.glb" />
+                            </div>
+                        </TabPane>
+                    </Tabs>
+                </div>
+            )
+
+        }
     }
 }
 
--- src/pages/productDetailPage.js
+++ src/pages/productDetailPage.js
@@ -27,20 +27,12 @@ function onQuantityChange(value) {
 }
 
 
-
-
-// Load Product Data
-//const getProductData = async () => {  
-//} 
-
-
-
 function ProductDetailPage({ history }){
     
-    /*
-    const [data, setData] = useState(null);
+    
+    const [productInfo, setProductInfo] = useState(0);
       
-    const getProductData = async() => {
+    const getProductData = async(code) => {
         try 
         {
             const response = await axios.get(`/api/product`, 
@@ -48,16 +40,13 @@ function ProductDetailPage({ history }){
                 params: 
                 {
                     sessionKey: sessionStorage.getItem("sessionKey"),
-                    productCode: '01194'
+                    productCode: code
                 }
             }, 
             {
                 headers: { 'Content-Type': 'application/JSON; charset=UTF-8' }
             })
-
-            console.log(response);
-
-            this.setData({data:    response.data.data})
+            setProductInfo(response.data.data)
         } 
         catch (err) 
         {
@@ -65,135 +54,111 @@ function ProductDetailPage({ history }){
         }
     }
     
-    getProductData();
-    */
-    const handleClick = ({ key }) => {
-        // TODO: Fix this to handle logout call
-        if (key.startsWith('/')) {
-            history.push(key)
-        }
-    }
+    useEffect(() => {
+        getProductData('01200');
+    },[]);
 
+
+    
     if(sessionStorage.getItem('user')){
-        //Layout
-        return (
-            <>
-            <Layout style={{ minHeight: '100vh' }}>
+        
+        if(productInfo==null) 
+            return (<>Loading Product Info...</>)
+        else
+            console.log("Loaded Product Info: ");
+            console.log(productInfo)
+            return (
+                <Layout style={{ minHeight: '100vh' }}>
 
-                {/* Top navigation bar */}
-                <NavigationBar  history={history} defaultSelected='/product'/>
-                
+                    {/* Top navigation bar */}
+                    <NavigationBar  history={history} defaultSelected='/product'/>
+                    
+                    {/* Main Content */}
+                    <div style={{ marginTop: '50px'}}>
+                    <Content style={{ padding: '50px 50px'}}> 
+                        <div style = {{borderRadius: "2%" , backgroundColor:'white'}} className={style.site_layout_content}>
 
-                <div style={{ marginTop: '50px'}}>
-                <Content style={{ padding: '50px 50px'}}> 
-                    <div style = {{borderRadius: "2%" , backgroundColor:'white'}} className={style.site_layout_content}>
-
-                        {/* Title */}
-                        <Row  gutter={[16, 16]}>
-                            <Col flex={7} >
-                                <div> <h2 style={{fontFamily: "Open Sans"}}> CFP - 600/12 Swirl Diffusers  with  Low Profile Plenum 250 Spigot</h2> </div>
-                            </Col>
-                        </Row>
-                        <Row  gutter={[16, 16]}>
-
-                            {/* Image Viewer */}
-                            <Col flex={9} >
-                                {/*<div style={{backgroundColor: "lightblue",minHeight:400}}> Image Viewer Component</div>*/}
-                                <ImageViewer height={400} width={700} imageList={null}/>
-                            </Col>
-
-                            {/* CheckOut Box */}
-                            <Col flex={3} >
-                                <div style={{backgroundColor: "whiteSmoke", borderRadius: "5%"}}>  
-
-                                    <div style={{padding: 20}}>
-                                        <Statistic title="Total Price (AUD)" value={25.5} prefix="$" precision={2} />
-                                    </div>
-                                    <div style={{paddingLeft: 20}}> 
-                                        <span> Quantity: </span>
-                                        <InputNumber min={1} max={10} defaultValue={1} onChange={onQuantityChange} />
-                                    </div>
-                                    <div style={{padding: 20}}>
-                                        <Button icon={<ShoppingCartOutlined />}>CheckOut</Button>
-                                    </div>
-                                
-                                </div>
-                            </Col>
-                        </Row>
-                        
-
-
-                        <div style={{ padding: '40px 16px' }}>
-                            <Divider orientation="left">Extra Information</Divider>
-                            <Row gutter={[16, 16]}>
-                            {/*    <Col span={4} offset={1}>
-                                    <div > 
-                                        Detail 1: Dummy <br/> 
-                                        Detail 2: Dummy <br/> 
-                                        Detail 3: Dummy <br/> 
-                                    </div>
+                            {/* Title */}
+                            <Row  gutter={[16, 16]}>
+                                <Col flex={7} >
+                                    <div style={{ marginLeft: '120px' }}> <h2 style={{fontFamily: "Open Sans"}}> {productInfo.productName} </h2> </div>
                                 </Col>
-                                <Col span={4}>
-                                    <div > 
-                                        Detail 4: Dummy <br/> 
-                                        Detail 5: Dummy <br/> 
-                                        Detail 6: Dummy <br/> 
-                                    </div>
-                                </Col>
-                            */}
-                             {/*   <Descriptions  bordered size="small" layout="horizontal" column={2}>
-                                
-                                    <Descriptions.Item key={1} label ={"Name"} span={2}> 600/12 Swirl Diffusers  with  Low Profile Plenum 250 Spigot </Descriptions.Item>
-                                    <Descriptions.Item key={2} label ={"URL"} span={2}> http://www.holyoake.com </Descriptions.Item>
-                                    <Descriptions.Item key={3} label ={"Static Pressure Min"}> 2 Pa </Descriptions.Item>
-                                    <Descriptions.Item key={4} label ={"Static Pressure Max"}> 28 Pa </Descriptions.Item>
-                                    <Descriptions.Item key={5} label ={"Noise Level NC Min"}> 5 NC </Descriptions.Item>
-                                    <Descriptions.Item key={6} label ={"Noise Level NC Max"}> 32NC </Descriptions.Item>
-                                
-                                </Descriptions>
-                            */}
-                                <Descriptions bordered size="small" layout="horizontal" column={2}>
-                                {
-                                    Object.entries(productDataSource).map(([param, value]) => {
-                                    return (
-                                        <Descriptions.Item key={param} label={param.replace(/##[\w]*/g, "")}>
-                                        {value}
-                                        </Descriptions.Item>
-                                    ) 
-                                    })
-                                }
-                                </Descriptions>
                             </Row>
-                        </div>
 
+                            {/* Image Viewer and Checkout box */}
+                            <Row  gutter={[16, 16]}>
 
-                        <div style={{ padding: '25px 16px' }}>
-                            <Row  gutter={[16, 16]} style={{minHeight:250}}>
-                            <Col flex={1}>
-                                <Tabs defaultActiveKey="1" onChange={callback}>
-                                    <TabPane tab="Description" key="1">
-                                        Radial Swirl Diffusers, Ceiling Fixed Pattern shall be Holyoake Model CFP-600/12.  Ceiling Radial Swirl Diffusers shall be designed for use in Variable Air Volume (VAV) systems with Highly Turbulent Radial  Air Flow Pattern and shall be suitable for ceiling heights of 2.4 to 4m. Ceiling Radial Swirl Diffusers shall maintain a COANDA effect at reduced air volumes and provide uniform temperature gradients throughout the occupied space. Diffusers shall be finished in powder coat and fitted with accessories and dampers where indicated as manufactured by Holyoake
-                                    </TabPane>
-                                    <TabPane tab="Specification" key="2">
+                                {/* Image Viewer */}
+                                <Col flex={9} >
+                                    <ImageViewer height={400} width={700} imageList={productInfo.imageList}/>
+                                </Col>
+
+                                {/* CheckOut Box */}
+                                <Col flex={3} >
+                                    <div style={{backgroundColor: "whiteSmoke", borderRadius: "5%"}}>  
+
+                                        <div style={{padding: 20}}>
+                                            <Statistic title="Total Price (AUD)" value={productInfo.price} prefix="$" precision={2} />
+                                        </div>
+                                        <div style={{paddingLeft: 20}}> 
+                                            <span> Quantity: </span>
+                                            <InputNumber min={1} max={10} defaultValue={1} onChange={onQuantityChange} />
+                                        </div>
+                                        <div style={{padding: 20}}>
+                                            <Button icon={<ShoppingCartOutlined />}>CheckOut</Button>
+                                        </div>
                                     
-                                    </TabPane>
-                                    <TabPane tab="Downloads" key="3">
-                                    
-                                    </TabPane>
-                                </Tabs>
-                            </Col>
-                        </Row>
-                        
-                        </div>
+                                    </div>
+                                </Col>
+                            </Row>
+                            
 
+                            {/* Extra Information */}
+                            <div style={{ padding: '30px 16px' }}>
+                                <Divider orientation="left" style={{ paddingBottom: '40px' }} >Extra Information</Divider>
+                                <Row gutter={[16, 16]}>
+                                    <Descriptions bordered size="small" layout="horizontal" column={2}>
+                                    {
+                                        Object.entries(productDataSource).map(([param, value]) => {
+                                        return (
+                                            <Descriptions.Item key={param} label={param.replace(/##[\w]*/g, "")}>
+                                            {value}
+                                            </Descriptions.Item>
+                                        ) 
+                                        })
+                                    }
+                                    </Descriptions>
+                                </Row>
+                            </div>
+
+                            {/* Tabs */}
+                            <div style={{ padding: '25px 16px' }}>
+                                <Row  gutter={[16, 16]} style={{minHeight:250}}>
+                                <Col flex={1}>
+                                    <Tabs defaultActiveKey="1" onChange={callback}>
+                                        <TabPane tab="Description" key="1">
+                                            {productInfo.description1}
+                                        </TabPane>
+                                        <TabPane tab="Specification" key="2">
+                                        
+                                        </TabPane>
+                                        <TabPane tab="Downloads" key="3">
+                                        
+                                        </TabPane>
+                                    </Tabs>
+                                </Col>
+                            </Row>
+                            
+                            </div>
+
+                        </div>
+                    </Content>
                     </div>
-                </Content>
-                </div>
-                                
-                <Footer style={{ textAlign: 'center' }}>SQUIZZ ©2020 Created by SQ-Wombat and SQ-Koala</Footer>
-            </Layout>
-            </>
-        )
+
+                    {/* Footer */}
+                    <Footer style={{ textAlign: 'center' }}>SQUIZZ ©2020 Created by SQ-Wombat and SQ-Koala</Footer>
+                </Layout>
+            )
     }
     //console.log(this.props.history)
     this.props.history.push('/login')
@@ -225,6 +190,6 @@ const productDataSource = {
     "Diffuser Width##LENGTH##MILLIMETERS": "595.00",
     "Plenum Box Width##LENGTH##MILLIMETERS": "570.00",
     //"Description##OTHER##": " Radial Swirl Diffusers, Ceiling Fixed Pattern shall be Holyoake Model CFP-600/12.  Ceiling Radial Swirl Diffusers shall be designed for use in Variable Air Volume (VAV) systems with Highly Turbulent Radial  Air Flow Pattern and shall be suitable for ceiling heights of 2.4 to 4m. Ceiling Radial Swirl Diffusers shall maintain a COANDA effect at reduced air volumes and provide uniform temperature gradients throughout the occupied space. Diffusers shall be finished in powder coat and fitted with accessories and dampers where indicated as manufactured by Holyoake"
-  }
+}
 
 export default withRouter(ProductDetailPage)
