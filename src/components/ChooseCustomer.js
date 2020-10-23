0033955794ddbc49e58917babf5fba74e7613ad3
huangzh521 <62923998+huangzh521@users.noreply.github.com>
2020-10-23 15:59:24 +0800

    class to function
    class to function

--- src/components/ChooseCustomer.js
+++ src/components/ChooseCustomer.js
@@ -1,90 +1,70 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import axios from 'axios';
 import {withRouter, Redirect} from 'react-router-dom'
 import { Button, Card, Form, List } from 'antd';
 import 'antd/dist/antd.css';
-
 import { useStoreState, useStoreActions } from 'easy-peasy';
 
-class ChooseCustomer extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            users: [],
-            redirect: false
-        }
-    }
 
-    // get all customers info
-    componentDidMount() {
-        axios({
-                method: 'get',
-                url: 'api/customers',
-                headers: {'Content-Type': 'application/JSON; charset=UTF-8'},
 
-            }
-        )
-            .then(
-                (response)=>{
-                    console.log("Get category list");
-                    console.log(response.data);
-                    this.setState({
-                        users: response.data,
-                    });
+const ChooseCustomer = ({ props }) => {
+    const [users, setUsers] = useState([])
+    const [redirect, setRedirect] = useState(false);
+    const { customerId } = useStoreState(state => ({
+        customerId: state.cart.customerId
+      }))
+    
+      // Global cart actions
+    const { setCustomerId, removeCustomerId } = useStoreActions(actions => ({
+        setCustomerId: actions.cart.setCustomerId,
+        removeCustomerId: actions.cart.removeCustomerId,
+      }))
+    
+    let isRendered = useRef(false);
+    useEffect(() => {
+        isRendered = true;
+        axios
+            .get("api/customers")
+            .then(res => {
+                if (isRendered) {
+                    console.log(res.data);
+                    setUsers(res.data);
                 }
-            )
-            .catch(function (error) {
-                console.log(error);
+                return null
             })
+            .catch(err => console.log(err));
+        return () => {
+            isRendered = false;
+        };
+    }, []);
+
+    if (redirect === true){
+        return <Redirect to = {{ pathname: "/" }} />
     }
+    
+    return (
+        <Card bordered={false} style={{ width: 300 }} cover={<img alt="example" src="https://media-exp1.licdn.com/dms/image/C511BAQF1N9JzP5PU8Q/company-background_10000/0?e=2159024400&v=beta&t=SogtI3ymEudS4fqNFeyKMxH7j5-2i7R1kH9LndNbPTg" />}>
+            <Form
+            className="chooseCustomer-form"
+            initialValues={{remember: true}}
+            >   
+                <List
+                    dataSource={users}
+                    renderItem={item => 
+                        <List.Item>
+                            <Button type="primary" className="customer-button" block onClick={() => {console.log(item.id);setCustomerId(item.id);setRedirect(true)}}>
+                                {item.customer_code}
+                            </Button>
+                        </List.Item>}
+                />
+                <Button type="link" block href='/create'>
+                    Create a customer account
+                </Button>
 
-    //Click on a customer code, its infomation will be return
-    //This still will be update since each customer has different url
-    getCustomer = (name, e) => {
-        console.log(name);
-        this.setState({
-            redirect: true,
-        });
-    }
+            </Form>
 
-    setCustomerId(e){
-        setCustomerId = useStoreActions(actions => ({
-            setCustomerId: actions.cart.setCustomerId
-        }))
-        console.log("Test Purpose "+e);
-        console.log(this.customerId)
-    }
-
-    render() {
-        if (this.state.redirect === true){
-            return <Redirect to = {{ pathname: "/" }} />
-        }
-
-        return (
-            <Card bordered={false} style={{ width: 300 }} cover={<img alt="example" src="https://media-exp1.licdn.com/dms/image/C511BAQF1N9JzP5PU8Q/company-background_10000/0?e=2159024400&v=beta&t=SogtI3ymEudS4fqNFeyKMxH7j5-2i7R1kH9LndNbPTg" />}>
-                <Form
-                className="chooseCustomer-form"
-                initialValues={{remember: true}}
-                >
-                    <List
-                        dataSource={this.state.users}
-                        renderItem={item =>
-                            <List.Item>
-                                <Button type="primary" className="customer-button" block onClick={this.getCustomer.bind(this, item.id)}>
-                                    {item.customer_code}
-                                </Button>
-                            </List.Item>}
-                    />
-                    <Button type="link" block href='/create'>
-                        Create a customer account
-                    </Button>
-
-                </Form>
-
-            </Card>
-        )
-    }
+        </Card>
+    )
 }
 
-
-export default ChooseCustomer
+export default withRouter(ChooseCustomer)
