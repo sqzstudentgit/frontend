3660a55dde8656c2a85884410886cf7d1370edb0
huangzh521 <62923998+huangzh521@users.noreply.github.com>
2020-10-27 11:24:32 +0800

    Revert "Revert "Merge branch 'Address' of https://github.com/ansabkhaliq/frontend into Address""
    This reverts commit 342189b27bb5963c9b3d8bb174c5dce8e7cc0e78.

--- src/pages/CheckOutPage.js
+++ src/pages/CheckOutPage.js
@@ -33,10 +33,11 @@ const CheckOutPage = ({ history }) =>{
     const [instruction, setInstruction] = useState("");
 
     // Global customer & cart state
-    const { customerId, products, totalPrice, deliveryAddrId, billingAddrId } = useStoreState(state => ({
+    const { customerId, products, totalPrice, afterGstPrice, deliveryAddrId, billingAddrId } = useStoreState(state => ({
         customerId: state.customer.customerId,
         products: state.cart.products,
         totalPrice: state.cart.totalPrice,
+        afterGstPrice: state.cart.afterGstPrice,
         deliveryAddrId: state.customer.deliveryAddrId,
         billingAddrId: state.customer.billingAddrId,
     }))
@@ -116,9 +117,7 @@ const CheckOutPage = ({ history }) =>{
     return(
         <Layout style={{ minHeight: '100vh' }}>
             {/* Top navigation bar */}
-            <Row justify="center" gutter={[0, 16]}>
-                <NavigationBar history={history} defaultSelected='/order'/>
-            </Row>
+            <NavigationBar history={history}/>
 
             {/* Add product form and cart information */}
             <Row style={{marginTop:'80px'}} justify="center" gutter={[0, 16]}>
@@ -144,7 +143,7 @@ const CheckOutPage = ({ history }) =>{
                                         </Button>
                                     </Col>
                                     <Col span={12}>
-                                        <Statistic title="GST" value={0} prefix="$" precision={2} />
+                                        <Statistic title="GST" value={afterGstPrice} prefix="$" precision={2} />
                                         <Button style={{ marginTop: 16 }} type="primary" onClick={() => handleSubmit()} loading={submitLoading}>
                                             Submit Order
                                         </Button>
